6af08a53a9ec23e78010ca2893b29cf0cb5adf6a
Alexey Skoblikov <happycow32@yandex.ru>
2022-09-02 23:37:30 +0300

    Task 58 multimatrix

---
+++ task58_multimatrix/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int [,] matrix1 = {{2, 4},
+                   {3, 2}};
+
+int [,] matrix2 = {{3, 4},
+                   {3, 3}};
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] MultiMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int rows = matrix1.GetLength(0);
+    int columns = matrix1.GetLength(1);
+    int[,] resultMatrix = new int[rows, columns];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix1.GetLength(1); j++)
+        {
+            resultMatrix[i, j] = matrix1[i, 0] * matrix2[0, j] + matrix1[i, 1] * matrix2[1, j];
+        }
+    }
+    return resultMatrix;
+}
+
+PrintMatrix(matrix1);
+Console.WriteLine();
+PrintMatrix(matrix2);
+Console.WriteLine();
+PrintMatrix(MultiMatrix(matrix1, matrix2));
