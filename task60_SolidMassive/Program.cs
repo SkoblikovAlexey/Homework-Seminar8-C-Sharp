156499bd1679dce822770c5be11d2960e66aced6
Alexey Skoblikov <happycow32@yandex.ru>
2022-09-03 23:46:48 +0300

    Task 60 Solid Massive finally comlete

--- task60_SolidMassive/Program.cs
+++ task60_SolidMassive/Program.cs
@@ -26,11 +26,11 @@ int[,,] CreateArray(int m, int n, int l)
 
 void PrintArray(int[,,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int k = 0; k < array.GetLength(2); k++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int i = 0; i < array.GetLength(0); i++)
         {
-            for (int k = 0; k < array.GetLength(2); k++)
+            for (int j = 0; j < array.GetLength(1); j++)
             {
                 Console.Write($"{array[i, j, k]} ({i}, {j}, {k}) ");
             }
